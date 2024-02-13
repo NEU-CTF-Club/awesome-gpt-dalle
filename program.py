d365b76592b4a54891fb89a5e3489cc873634879
floor3d <81770879+floor3d@users.noreply.github.com>
2024-02-13 18:48:00 -0500

    it works :cool face:
    pog
    
    Signed-off-by: floor3d <81770879+floor3d@users.noreply.github.com>

--- program.py
+++ program.py
@@ -1,5 +1,12 @@
-main():
-  # TODO : Everything
-  print("hello world!")
+from openai import OpenAI
 
-main()
+client = OpenAI(api_key="sk-YouWinAndFoundMyGPTKey!GoGetY0urPrize")
+
+prompt = input("Enter your image prompt: ")
+
+print(client.images.generate(
+  model="dall-e-3",
+  prompt=prompt,
+  n=1,
+  size="1024x1024"
+))
